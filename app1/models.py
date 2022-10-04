a5f4de64beea884e46ba1542a2d0898fa3e4fa24
amineid12 <amin.eid@axsos.me>
2022-10-04 22:12:51 +0300

    commit

---
+++ app1/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class BookManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # add keys and values to errors dictionary for each invalid field
+        if len(postData['title']) < 5:
+            errors["title"] = "Book name should be at least 5 characters"
+
+        return errors
+
+class Book(models.Model):
+	title = models.CharField(max_length=255)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	objects=BookManager()
+	
+	
+class Publisher(models.Model):
+	name = models.CharField(max_length=255)
+	books = models.ManyToManyField(Book, related_name="publishers")
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+
